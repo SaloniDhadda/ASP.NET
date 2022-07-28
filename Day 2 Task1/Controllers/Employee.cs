0c54f8f8f51fa461cfa18a3cbf9fc6091e5079b8
SaloniDhadda <73308690+SaloniDhadda@users.noreply.github.com>
2022-07-28 10:16:20 +0530

    Add files via upload

---
+++ Day 2 Task1/Controllers/Employee.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WebApplication2.Controllers
+{
+    public class Employee : Controller
+    {
+        DbManager context = new DbManager();
+        public IActionResult Index()
+        {
+            List<employee> empList = context.GetAllDetails();
+            return View(empList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(employee e)
+        {
+            if (ModelState.IsValid == true)
+            {
+                context.AddEmp(e);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Requesttype = Request.Method;
+                ViewBag.ErrorMessage = "Invalid Details";
+                return View();
+            }
+
+        }
+        public IActionResult Details(int Id)
+        {
+            employee e1 = context.GetDetailsByID(Id);
+            return View(e1);
+        }
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            employee e1 = context.GetDetailsByID(Id);
+            return View(e1);
+        }
+        [HttpPost]
+        public IActionResult Edit(employee e1)
+        {
+            if (ModelState.IsValid == true)
+            {
+                context.UpdateEmp(e1);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Requesttype = Request.Method;
+                ViewBag.ErrorMessage = "Invalid Details";
+                return View();
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            employee e1 = context.GetDetailsByID(Id);
+            return View(e1);
+        }
+        [HttpPost]
+        public IActionResult deleteConfirm(int Id)
+        {
+            context.DeleteEmp(Id);
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
+
+
---
+++ Day 2 Task1/Models/DbManager.cs
@@ -0,0 +1,42 @@
+﻿namespace WebApplication2.Models
+{
+    public class DbManager
+    {
+        public static List<employee> empList;
+
+        public DbManager()
+        {
+            empList = new List<employee>() {
+                new employee(){ Id=1011,Name="Jack",job="SoftwareEngineer",sal=90000,deptid=1},
+                new employee(){ Id=1012,Name="Joe",job="SoftwareEngineer",sal=27000,deptid=2},
+                new employee(){ Id=1013,Name="Luke",job="IT Trainee",sal=25000,deptid=5},
+                new employee(){ Id=1014,Name="Jay",job="Intern",sal=9000,deptid=5},
+            };
+        }
+
+        public List<employee> GetAllDetails()
+        {
+            return empList;
+        }
+
+        public void AddEmp(employee e1)
+        {
+            empList.Add(e1);
+        }
+        public employee GetDetailsByID(int Id)
+        {
+            return empList.Find(item => item.Id == Id);
+        }
+        public void DeleteEmp(int Id)
+        {
+            employee e1 = empList.Find(item => item.Id == Id);
+            empList.Remove(e1);
+        }
+        public void UpdateEmp(employee updatedobj)
+        {
+            employee oldobj = empList.Find(item => item.Id == updatedobj.Id);
+            empList.Remove(oldobj);
+            empList.Add(updatedobj);
+        }
+    }
+}
