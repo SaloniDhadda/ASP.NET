77988299c37448277e675d709207188434160013
SaloniDhadda <73308690+SaloniDhadda@users.noreply.github.com>
2022-07-29 16:45:08 +0530

    Add files via upload

---
+++ Day 5 - Task/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+        {
+            EmployeeDbContext _context;
+            public EmployeesController(EmployeeDbContext context)
+            {
+                _context = context;
+            }
+
+            [HttpGet]
+            public async Task<IActionResult> GetAllEmployees()
+            {
+                var stdList = await _context.Employees.ToListAsync();
+                return Ok(stdList);
+            }
+
+
+            [HttpGet("{id}")]
+            public async Task<IActionResult> GetEmployeeById(int id)
+            {
+                var stdObj = await _context.Employees.FindAsync(id);
+
+                if (stdObj != null)
+                    return Ok(stdObj);
+                else
+                    return NotFound("Requested Employee Id does not exists.");
+            }
+
+
+            [HttpPost]
+            public async Task<IActionResult> AddEmployee(Employee obj)
+            {
+                await _context.Employees.AddAsync(obj);
+                await _context.SaveChangesAsync();
+                return Ok("New Employee details are saved in database.");
+            }
+
+            [HttpPut]
+            public async Task<IActionResult> UpdateEmployee(Employee obj)
+            {
+                _context.Employees.Update(obj);
+                await _context.SaveChangesAsync();
+                return Ok("Employee details are updated in database.");
+            }
+
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> DeleteEmployee(int id)
+            {
+                var stdObj = await _context.Employees.FindAsync(id);
+                _context.Employees.Remove(stdObj);
+                await _context.SaveChangesAsync();
+                return Ok("Employee details are deleted from database.");
+            }
+        }
+    }
---
+++ Day 5 - Task/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Sal { get; set; }
+        public int Deptno { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
